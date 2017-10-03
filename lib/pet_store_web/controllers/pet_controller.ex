0d589a3a4d40cb21175cd1f09f70e0db9ed57f44
Chris Jewell <chrisjohnjewell@gmail.com>
2017-10-03 16:55:28 +1100

    Add a basic JSON API 404 for showing a pet

--- lib/pet_store_web/controllers/pet_controller.ex
+++ lib/pet_store_web/controllers/pet_controller.ex
@@ -1,6 +1,7 @@
 defmodule PetStoreWeb.PetController do
   use PetStoreWeb, :controller
   alias PetStore.Pets
+  alias PetStoreWeb.ErrorView
 
   def index(conn, _params) do
     pets = Pets.list_pets
@@ -8,7 +9,16 @@ defmodule PetStoreWeb.PetController do
   end
 
   def show(conn, %{"id" => id}) do
-    pet = Pets.get_pet!(id)
-    render conn, pet: pet
+    case Pets.get_pet(id) do
+      nil -> render_error(conn, :not_found)
+      pet -> render(conn, pet: pet)
+    end
+  end
+
+  defp render_error(conn, :not_found) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(ErrorView)
+    |> render("404.json", [])
   end
 end
--- lib/pet_store_web/controllers/pet_controller_test.exs
+++ lib/pet_store_web/controllers/pet_controller_test.exs
@@ -1,6 +1,7 @@
 defmodule PetStoreWeb.PetControllerTest do
   use PetStoreWeb.ConnCase
   alias PetStoreWeb.PetView
+  alias PetStoreWeb.ErrorView
 
   test "#index renders a list of pets" do
     conn = build_conn()
@@ -12,13 +13,24 @@ defmodule PetStoreWeb.PetControllerTest do
       render_jsonapi(PetView, "index.json", %{pets: [pet]})
   end
 
-  test "#show renders a specific pet" do
-    conn = build_conn()
-    pet = insert(:pet)
+  describe "#show do" do
+    test "Success renders a specific pet" do
+      conn = build_conn()
+      pet = insert(:pet)
 
-    conn = get conn, pet_path(conn, :show, pet)
+      conn = get conn, pet_path(conn, :show, pet)
 
-    assert json_response(conn, 200) ==
-      render_jsonapi(PetView, "show.json", %{pet: pet})
+      assert json_response(conn, 200) ==
+        render_jsonapi(PetView, "show.json", %{pet: pet})
+    end
+
+    test "Not Found renders a JSON API 404" do
+      conn = build_conn()
+
+      conn = get conn, pet_path(conn, :show, 123)
+
+      assert json_response(conn, 404) ==
+        render_jsonapi(ErrorView, "404.json", %{})
+    end
   end
 end
