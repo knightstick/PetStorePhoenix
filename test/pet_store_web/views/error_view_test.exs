74b2919878552db701fcc7275a6820bd7b7b771e
Chris Jewell <chrisjohnjewell@gmail.com>
2017-10-03 16:54:14 +1100

    Add a JSON API 404.json in ErrorView

--- test/pet_store_web/views/error_view_test.exs
+++ test/pet_store_web/views/error_view_test.exs
@@ -4,9 +4,16 @@ defmodule PetStoreWeb.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
+  # Would be nice to pass through the detail
   test "renders 404.json" do
-    assert render(PetStoreWeb.ErrorView, "404.json", []) ==
-           %{errors: %{detail: "Page not found"}}
+    assert %{errors: [error | []]} =
+      render(PetStoreWeb.ErrorView, "404.json", [])
+
+    %{status: status_string, title: title, detail: detail} = error
+
+    assert status_string == "404"
+    assert title == "Not Found"
+    assert detail == "Not Found"
   end
 
   test "render 500.json" do
