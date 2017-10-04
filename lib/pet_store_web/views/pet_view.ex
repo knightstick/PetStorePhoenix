646d43ffcf5fd2bda03dffd327fd5eb0726945ce
Chris Jewell <chrisjohnjewell@gmail.com>
2017-10-04 17:38:11 +1100

    Use JaSerializer in PetView

--- lib/pet_store_web/views/pet_view.ex
+++ lib/pet_store_web/views/pet_view.ex
@@ -1,26 +1,7 @@
 defmodule PetStoreWeb.PetView do
-  def render("index.json", %{pets: pets}) do
-    %{
-      data: Enum.map(pets, &pet_jsonapi/1)
-    }
-  end
+  use PetStoreWeb, :view
+  use JaSerializer.PhoenixView
 
-  def render("show.json", %{pet: pet}) do
-    %{
-      data: pet_jsonapi(pet)
-    }
-  end
-
-  # Guard or similar to only render pets?
-  # Use a library to "adapt" to JSON API?
-  # Maybe have an adapter as a module attribute?
-  def pet_jsonapi(pet) do
-    %{
-      type: "pets",
-      id: pet.id,
-      attributes: %{
-        name: pet.name
-      }
-    }
-  end
+  location "/pets/:id"
+  attributes [:name]
 end
