415543ff3da464ebf9fcf5c7eff74769d53a9c9e
Chris Jewell <chrisjohnjewell@gmail.com>
2017-10-11 15:06:27 +1100

    Try building within buildkite

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -1,1 +1,3 @@
-echo 'Building releases!!!'
+echo "Building pet_store_build:0.1${BUILDKITE_COMMIT}"
+mix docker.build --tag pet_store_build:0.1${BUILDKITE_COMMIT} &&
+  mix docker.release --tag pet_store:0.1${BUILDKITE_COMMIT}
