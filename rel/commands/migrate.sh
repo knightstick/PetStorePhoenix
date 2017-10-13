e1f0d68662dc3d1fc510cf84919063aac3264541
Chris Jewell <chrisjohnjewell@gmail.com>
2017-10-13 11:58:48 +1100

    Add migrate release task into executable

---
+++ rel/commands/migrate.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+$RELEASE_ROOT_DIR/bin/pet_store command Elixir.PetStore.ReleaseTasks seed
--- rel/config.exs
+++ rel/config.exs
@@ -49,5 +49,8 @@ release :pet_store do
   set applications: [
     :runtime_tools
   ]
+  set commands: [
+    "migrate": "rel/commands/migrate.sh"
+  ]
 end
 
